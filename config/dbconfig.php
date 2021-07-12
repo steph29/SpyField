d4813e1f996bda8f522e25ab218ff55a17597843
Stephane Verardo <s.verardo29@gmail.com>
2021-07-12 11:10:04 +0200

    Firebase configuration done. Insert OK

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -4,7 +4,7 @@ require '../vendor/autoload.php';
 
 use Kreait\Firebase\Factory;
 
-$factory = (new Factory)->withServiceAccount('spyfield-b2064-firebase-adminsdk-56zwj-0cf183d972.json')
+$factory = (new Factory)->withServiceAccount('/Users/stephaneverardo/SpyField/config/spyfield-b2064-firebase-adminsdk-56zwj-0cf183d972.json')
 ->withDatabaseUri('https://spyfield-b2064-default-rtdb.europe-west1.firebasedatabase.app/');
     $database = $factory->createDatabase();
 
