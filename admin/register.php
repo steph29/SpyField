85b3cdc23e3b916fe4583670b04e7c92fa4e1f53
Stephane Verardo <s.verardo29@gmail.com>
2021-07-12 16:34:35 +0200

    fix bugs wtih formular

--- admin/register.php
+++ admin/register.php
@@ -27,20 +27,19 @@ $pageDesc = "Inscrivez vous! Pour chnager le monde, creer des missions, espionne
 <form action="signin" method="POST">
 <div class="form-group mb-3">
 <label for="">Full name</label>
-<input type="text" name="fullname" class="form-control">
+<input type="text" name="fullname" class="form-control" placeholder="Jean Bond">
 </div>
-
 <div class="form-group mb-3">
 <label for="">Password</label>
 <input type="password" name="password" class="form-control">
 </div>
 <div class="form-group mb-3">
 <label for="">Email</label>
-<input type="email" name="email" class="form-control">
+<input type="email" name="email" class="form-control" placeholder="jean.bond@secret.com">
 </div>
 <div class="form-group mb-3">
 <label for="">Phone Number</label>
-<input type="number" name="phone" class="form-control">
+<input type="number" name="phone" class="form-control" placeholder="123456789">
 </div>
 
 <div class="form-group mb-3">
