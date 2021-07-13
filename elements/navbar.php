d4ae1c337c9256f4f2540f333acb16946e933145
Stephane Verardo <s.verardo29@gmail.com>
2021-07-13 11:13:59 +0200

    change navbar

--- elements/navbar.php
+++ elements/navbar.php
@@ -1,5 +1,7 @@
 <nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
+    <h2 class="navbar-brand mx-5" href="#">SPYFIELD</h2>
+
     <button
       class="navbar-toggler"
       type="button"
@@ -12,7 +14,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse mx-3" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link" href="<?= $router->generate('home') ?>">Home</a>
         </li>
