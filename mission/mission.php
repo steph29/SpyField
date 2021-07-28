458510987db20fd753ef2306b6b03f3bc19608f3
Stephane Verardo <s.verardo29@gmail.com>
2021-07-28 17:25:56 +0200

    add jquery on mission.php file

--- mission/mission.php
+++ mission/mission.php
@@ -52,6 +52,8 @@ $pageDesc = "Retrouvez ici toutes les informations sur vos missions"; ?>
         
     </div>
 </div>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
 <script src="ajax.js"></script>
 <script src="script.js"></script>
 
