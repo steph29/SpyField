b2cfa39d77c601d9faa6f612dfecb214ad99cd01
Stephane Verardo <s.verardo29@gmail.com>
2021-09-20 14:32:17 +0200

    remove home from navbar

--- elements/navbar.php
+++ elements/navbar.php
@@ -7,9 +7,9 @@
     </button>
     <div class="collapse navbar-collapse mx-3" id="navbarNav">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="<?= $router->generate('home') ?>">Home</a>
-        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link" href="<?= $router->generate('mission') ?>">Home</a>
+        </li> -->
         <li class="nav-item">
           <a class="nav-link" href="<?= $router->generate('mission')?>">Mission</a>
           <?php if(!isset($_SESSION[ 'verified_user_id'])) : ?>
