0a2e0adf59261f300a39c4b29f1a03d960f09084
Stephane Verardo <s.verardo29@gmail.com>
2021-09-27 15:29:53 +0200

    change CRUD name in to crud

--- config/routes.php
+++ config/routes.php
@@ -28,8 +28,8 @@ $router-> map('GET|POST', '/status', 'elements/status', 'status');
 
 $router-> map('GET|POST', '/register', 'admin/register', 'registerPage' );
 
-// CRUD
-$router-> map('GET|POST', '/edit', 'config/CRUD/edit', 'edit');
-$router-> map('GET|POST', '/delete', 'config/CRUD/delete', 'delete');
-$router-> map('GET|POST', '/update', 'config/CRUD/update', 'update');
-$router-> map('GET|POST', '/updateAgent', 'config/CRUD/updateAgent', 'updateAgent');
+// crud
+$router-> map('GET|POST', '/edit', 'config/crud/edit', 'edit');
+$router-> map('GET|POST', '/delete', 'config/crud/delete', 'delete');
+$router-> map('GET|POST', '/update', 'config/crud/update', 'update');
+$router-> map('GET|POST', '/updateAgent', 'config/crud/updateAgent', 'updateAgent');
