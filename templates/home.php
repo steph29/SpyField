30c4e289cf312ac5fa40dcfbe9b37dcaf1a387ca
Stephane Verardo <s.verardo29@gmail.com>
2021-09-27 16:30:15 +0200

    clear unuse files

--- templates/home.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$pageTitle = "Bienvenu";
-$pageDesc = "Ce site est destiné à tracé et suivre l'état de vos mission. Pour toutes questions, rendez-vous sur la page contact."; ?>
-<h1>This is my home, please coming! </h1>
